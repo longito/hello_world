e57d8c13f53cac19847c8caed8201ef165018144
loncbt <13996789263@163.com>
2026-06-10 15:04:41 +0800

    在分离头指针425d376上修改main.c增加参数的打印

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 int main(int argc,char **argv)
 {
-	printf("hello world\n");
+	if(argc >= 2 && argv[1] != NULL)
+	{
+		char *name = argv[1];
+		printf("hello world, this is  %s\n",name);
+	}
+	else
+		printf("hello world\n");
 	return 0;
 }
